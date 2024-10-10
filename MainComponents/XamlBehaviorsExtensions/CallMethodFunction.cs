5ada8cc177d7ff1c1b3840677aa712c009e8ac13
muhastepan <stiazhkinf@mail.ru>
2024-10-10 14:21:48 +0300

    feat: расширения xaml для вызова методов-функций

---
+++ MainComponents/XamlBehaviorsExtensions/CallMethodFunction.cs
@@ -0,0 +1,31 @@
+﻿using System.Windows;
+
+namespace MainComponents.XamlBehaviorsExtensions;
+
+public class CallMethodFunction : Microsoft.Xaml.Behaviors.TriggerAction<DependencyObject>
+{
+    public static readonly DependencyProperty TargetObjectProperty = DependencyProperty.Register(
+        nameof(TargetObject), typeof(FrameworkElement), typeof(CallMethodFunction), new PropertyMetadata(default(FrameworkElement)));
+
+    public FrameworkElement TargetObject
+    {
+        get { return (FrameworkElement)GetValue(TargetObjectProperty); }
+        set { SetValue(TargetObjectProperty, value); }
+    }
+
+    public static readonly DependencyProperty MethodNameProperty = DependencyProperty.Register(
+        nameof(MethodName), typeof(string), typeof(CallMethodFunction), new PropertyMetadata(default(string)));
+
+    public string MethodName
+    {
+        get { return (string)GetValue(MethodNameProperty); }
+        set { SetValue(MethodNameProperty, value); }
+    }
+
+    /// <inheritdoc />
+    protected override void Invoke(object parameter)
+    {
+        var method = TargetObject.GetType().GetMethod(MethodName);
+        method?.Invoke(TargetObject, null);
+    }
+}
