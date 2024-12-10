4425065c33b9dae48cdd1b01d80c213f92fc28ab
muhastepan <stiazhkinf@mail.ru>
2024-12-10 09:04:02 +0300

    feat: начальное время видео при перезапуске

--- MainComponents/Components/RepeatingMediaElement.cs
+++ MainComponents/Components/RepeatingMediaElement.cs
@@ -1,9 +1,19 @@
-﻿using System.Windows.Controls;
+﻿using System.Windows;
+using System.Windows.Controls;
 
 namespace MainComponents.Components;
 
 public class RepeatingMediaElement:MediaElement
 {
+    public static readonly DependencyProperty StartTimeProperty = DependencyProperty.Register(
+        nameof(StartTime), typeof(TimeSpan), typeof(RepeatingMediaElement), new PropertyMetadata(TimeSpan.Zero));
+
+    public TimeSpan StartTime
+    {
+        get { return (TimeSpan)GetValue(StartTimeProperty); }
+        set { SetValue(StartTimeProperty, value); }
+    }
+
     public RepeatingMediaElement()=>MediaEnded += RepeatingMediaElement_MediaEnded;
-    private void RepeatingMediaElement_MediaEnded(object sender, System.Windows.RoutedEventArgs e)=>Position = TimeSpan.Zero;
+    private void RepeatingMediaElement_MediaEnded(object sender, System.Windows.RoutedEventArgs e)=>Position = StartTime;
 }
