c648fbed9e691306b83db8f5b4edcd567dc99c94
muhastepan <stiazhkinf@mail.ru>
2024-12-18 16:07:51 +0300

    feat: изменена анимация

--- MainComponents/Popups/PopupContainers/ScaleContainer.cs
+++ MainComponents/Popups/PopupContainers/ScaleContainer.cs
@@ -6,44 +6,60 @@ namespace MainComponents.Popups.PopupContainers
 {
     public class ScaleContainer : BasePopupContainer
     {
-
         protected override void ContainerLoaded(object sender, RoutedEventArgs e)
         {
-            RenderTransform = new ScaleTransform(0, 0, ActualWidth / 2, ActualHeight / 2);
-            var xAnimation = new DoubleAnimation(1, Duration)
+            RenderTransform = new ScaleTransform(0.5, 0.5, ActualWidth / 2, ActualHeight / 2);
+            var xAnim = new DoubleAnimation(1, Duration)
             {
                 EasingFunction = EasingFunction
             };
-            var yAnimation = new DoubleAnimation(1, Duration)
+            var yAnim = new DoubleAnimation(1, Duration)
             {
                 EasingFunction = EasingFunction
             };
-            StartAnimation(xAnimation, yAnimation);
+            var oAnim = new DoubleAnimation(1, Duration)
+            {
+                EasingFunction = EasingFunction
+            };
+
+            StartAnimation(xAnim,yAnim,oAnim);
         }
 
         public override void Close()
         {
-            var xAnimation = new DoubleAnimation(0, Duration)
+            var xAnim = new DoubleAnimation(0.5, Duration)
             {
                 EasingFunction = EasingFunction
             };
-            var yAnimation = new DoubleAnimation(0, Duration)
+            var yAnim = new DoubleAnimation(0.5, Duration)
             {
                 EasingFunction = EasingFunction
             };
-            yAnimation.Completed += (o, args) => RaiseEvent(new RoutedEventArgs(ClosedEvent));
-            StartAnimation(xAnimation, yAnimation);
+            var oAnim = new DoubleAnimation(0, Duration)
+            {
+                EasingFunction = EasingFunction
+            };
+
+            
+            xAnim.Completed += (o, args) => RaiseEvent(new RoutedEventArgs(ClosedEvent));
+            StartAnimation(xAnim,yAnim,oAnim);
         }
 
         protected override void StartAnimation(params Timeline[] animations)
         {
             var sb = new Storyboard();
-            sb.Children.Add(animations[0]);
-            sb.Children.Add(animations[1]);
-            Storyboard.SetTarget(animations[0], this);
-            Storyboard.SetTarget(animations[1], this);
-            Storyboard.SetTargetProperty(animations[0], new PropertyPath("(UIElement.RenderTransform).(ScaleTransform.ScaleX)"));
-            Storyboard.SetTargetProperty(animations[1], new PropertyPath("(UIElement.RenderTransform).(ScaleTransform.ScaleY)"));
+            foreach (var animation in animations)
+            {
+                sb.Children.Add(animation);
+                Storyboard.SetTarget(animation, this);
+            }
+
+            Storyboard.SetTargetProperty(animations[0],
+                new PropertyPath("(UIElement.RenderTransform).(ScaleTransform.ScaleX)"));
+            Storyboard.SetTargetProperty(animations[1],
+                new PropertyPath("(UIElement.RenderTransform).(ScaleTransform.ScaleY)"));
+            Storyboard.SetTargetProperty(animations[2], new PropertyPath("(UIElement.Opacity)"));
+
             sb.Begin();
         }
     }
