bc03d3780f5afc02d3d5896505376a1df8c1ea93
ACESQ <al.snagg@gmail.com>
2025-06-05 13:07:19 +0400

    feat: Добавил AsyncImage

---
+++ MainComponents/Components/AsyncImage.xaml.cs
@@ -0,0 +1,86 @@
+﻿using System.IO;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using MainComponents.Helpers;
+
+namespace MainComponents.Components;
+
+/// <summary>
+/// Логика взаимодействия для AsyncImage.xaml
+/// </summary>
+public partial class AsyncImage : UserControl
+{
+    public static readonly DependencyProperty ImageScalingModeProperty = DependencyProperty.Register(nameof(ImageScalingMode), typeof(BitmapScalingMode), typeof(AsyncImage), new PropertyMetadata(default(BitmapScalingMode)));
+    
+
+    public static readonly DependencyProperty StretchProperty = DependencyProperty.Register(nameof(Stretch),
+        typeof(Stretch), typeof(AsyncImage), new PropertyMetadata(default(Stretch)));
+
+    public static readonly DependencyProperty ImageSourceProperty = DependencyProperty.Register(nameof(ImageSource),
+        typeof(string), typeof(AsyncImage), new PropertyMetadata(default(string), SourceChanged));
+
+    public string ImageSource
+    {
+        get => (string)GetValue(ImageSourceProperty);
+        set => SetValue(ImageSourceProperty, value);
+    }
+
+    public Stretch Stretch
+    {
+        get => (Stretch)GetValue(StretchProperty);
+        set => SetValue(StretchProperty, value);
+    }
+
+
+    public BitmapScalingMode ImageScalingMode
+    {
+        get => (BitmapScalingMode)GetValue(ImageScalingModeProperty);
+        set => SetValue(ImageScalingModeProperty, value);
+    }
+
+    public static readonly DependencyProperty RenderAtScaleProperty = DependencyProperty.Register(
+        nameof(RenderAtScale), typeof(double), typeof(AsyncImage), new PropertyMetadata(default(double)));
+
+    public double RenderAtScale
+    {
+        get { return (double)GetValue(RenderAtScaleProperty); }
+        set { SetValue(RenderAtScaleProperty, value); }
+    }
+
+    public AsyncImage()
+    {
+        InitializeComponent();
+    }
+
+
+    private static void SourceChanged(DependencyObject d, DependencyPropertyChangedEventArgs e) =>
+        ((AsyncImage)d).SourceChanged((string)e.NewValue);
+
+    private async void SourceChanged(string newSource)
+    {
+        if (string.IsNullOrEmpty(newSource)) return;
+        if (!File.Exists(newSource)) return;
+        var bmp = await ImageCache.LoadBitmap(
+            newSource,
+            Width is double.NaN ? 0 : (int)Width,
+            Height is double.NaN ? 0 : (int)Height,
+            ImageScalingMode);
+        if(bmp is null) return;
+        if (RenderAtScale == 0)
+        {
+            Content = new Image()
+            {
+                Source = bmp,
+                Stretch = Stretch
+            };
+            return;
+        }
+
+        Content = ImageCache.LoadImage(
+            bmp,
+            Stretch,
+            RenderAtScale);
+        
+    }
+}
---
+++ MainComponents/Helpers/ImageCache.cs
@@ -0,0 +1,65 @@
+﻿using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace MainComponents.Helpers;
+
+public static class ImageCache
+{
+
+    private static readonly Dictionary<BitmapImage, Image> Images = [];
+    private static readonly Dictionary<string, BitmapImage> BitmapImages = [];
+
+    public static void Clear()
+    {
+        Images.Clear();
+        BitmapImages.Clear();
+    }
+
+    public static async Task<BitmapImage?> LoadBitmap(string path, int width, int height, BitmapScalingMode scalingMode)
+    {
+        if (BitmapImages.TryGetValue(path, out var image)) return image;
+
+        var bmp = await Task.Run(
+            () =>
+            {
+                try
+                {
+                    var bitmap = new BitmapImage();
+                    bitmap.BeginInit();
+                    bitmap.DecodePixelWidth = width;
+                    bitmap.DecodePixelHeight = height;
+                    bitmap.CacheOption = BitmapCacheOption.None;
+                    bitmap.UriSource = new Uri(path);
+                    RenderOptions.SetBitmapScalingMode(bitmap, scalingMode);
+                    bitmap.EndInit();
+                    bitmap.Freeze();
+                    return bitmap;
+                }
+                catch
+                {
+                    return null;
+                }
+
+            });
+
+        if (bmp is null) return null;
+        BitmapImages.TryAdd(path, bmp);
+        return bmp;
+    }
+
+    public static Image? LoadImage(BitmapImage bmp, Stretch stretch, double scale)
+    {
+        if (Images.TryGetValue(bmp, out var image)) return image;
+
+        var newImage = new Image
+        {
+            Source = bmp,
+            Stretch = stretch,
+            CacheMode = new BitmapCache(scale)
+        };
+        
+        Images.TryAdd(bmp, newImage);
+        return newImage;
+    }
+}
